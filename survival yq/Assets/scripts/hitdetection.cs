e6dc83dec8f8c97dbf39a0b13e6bf38c149d2811
oskar ,ok <foldagerfamilie@gmail.com>
2022-08-13 21:35:41 +0200

    SHIT WORKING KINDA

--- survival yq/Assets/scripts/hitdetection.cs
+++ survival yq/Assets/scripts/hitdetection.cs
@@ -10,20 +10,20 @@ public class hitdetection : MonoBehaviour
     public static int points;
     public int enemystrength;
     public int enemysvalue;
+    public float period = 200f;
+    private float nextactiontime = 0;
+    private bool isconnecting = false;
+    public BoxCollider2D thisenemy;
+    public BoxCollider2D thisenemy2;
+    private BoxCollider2D player;
+    public static int heathl;
 
 
-    void OnTriggerEnter2D(Collider2D col)
+    void OnTriggerEnter2D(Collider2D col1)
     {
         Debug.Log("OnCollisionEnter2D");
-        if (col.gameObject.tag == "Player")
-        {
-            Debug.Log("hit");
-            Destroy(enemy);
-            healthpoints-=enemystrength;
-            GameObject.FindObjectOfType<healthbar>().kill();
-            Debug.Log(healthpoints);
-        }
-        if (col.gameObject.tag == "sword")
+
+        if (col1.gameObject.tag == "sword")
         {
             Debug.Log("hit");
             Destroy(enemy);
@@ -31,8 +31,45 @@ public class hitdetection : MonoBehaviour
 
         }
     }
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            isconnecting = true;
 
 
-    
+        } 
+    }
+
+
+    public void Update()
+    {
+        Debug.Log("time is " + nextactiontime);
+        heathl = enemystrength;
+        Debug.Log(isconnecting);
+        if (thisenemy.IsTouching(player) == false || thisenemy2.IsTouching(player) == false )
+        {
+            isconnecting = false;
+        }
+        if (isconnecting == true)
+        {
+            if (Time.time > nextactiontime)
+            {
+                Debug.Log("hitie");
+                //Destroy(enemy);
+                healthpoints -= enemystrength;
+                GameObject.FindObjectOfType<healthbar>().kill();
+                Debug.Log("heeelat is"+healthpoints);
+                nextactiontime += period;
+            }
+        }
+    }
+    public void Start()
+    {
+        player = fixshit.a;
+    }
+
+
+
 
 }
